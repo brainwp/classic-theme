b6a7431a0c06cdd160f8ba26f1837dbd89e2354f
guibressane <gui@brasa.art.br>
2014-04-29 23:50:16 -0300

    Pequenos acertos

--- page.php
+++ page.php
@@ -14,7 +14,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<div class="content-1000">
+			<div class="content-850">
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php get_template_part( 'content', 'page' ); ?>
