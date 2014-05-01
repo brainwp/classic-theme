802f13e6ed4d163b67d59bfc3871bde3d87d687f
everaldomatias <matiaseveraldo@gmail.com>
2014-05-01 12:45:56 -0300

    Aplicando PHP ns single-produtos.php

--- single-produtos.php
+++ single-produtos.php
@@ -1,32 +1,45 @@
-<?php
-/**
- * The Template for displaying all single posts.
- *
- * @package Classic Theme
- */
-
-get_header(); ?>
+<?php get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div class="content-1000">
+				<div class="produto-img">
+					<?php if ( has_post_thumbnail() ) {
+						the_post_thumbnail();
+						} else { ?>
+						<img src="<?php echo get_template_directory_uri(); ?>/images/default-classic-500.png" alt="<?php the_title(); ?>" />
+					<?php } ?>
+				</div><!-- .produto-img -->
+				<div class="caixa-produto-descricao">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'single' ); ?>
+					<?php get_template_part( 'content', 'page' ); ?>
 
-			<?php classic_theme_post_nav(); ?>
+					<?php endwhile; // end of the loop. ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
+				</div>
+			</div>
 
-		<?php endwhile; // end of the loop. ?>
+			<div class="posts-rel">
+				<h1>Produtos Relacionados</h1>
+				<ul>
+					<?php $relacionados = new WP_Query( array( 'post_type' => 'produtos', 'posts_per_page' => 4 ) ); ?>
+					<?php while ( $relacionados->have_posts() ) : $relacionados->the_post(); ?>
+					<?php if ( has_post_thumbnail() ) {
+						$thumb_relacionados = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+						} else {
+						$thumb_relacionados = get_template_directory_uri() . "/images/default-classic-500.png";
+					} ?>
+					<li><img src="<?php echo $thumb_relacionados; ?>">
+						<h2><?php the_title(); ?></h2>
+						<?php the_excerpt(); ?>
+					</li>
 
+					<?php endwhile; wp_reset_query(); ?>
+				</ul>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
